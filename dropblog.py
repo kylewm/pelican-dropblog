fbfba462486cbf553f95b8ec52f8cdd076e5a2d9
kylewm <kyle.mahan@gmail.com>
2013-07-12 19:49:21 -0700

    Use the correct comment format for Python...

--- dropblog.py
+++ dropblog.py
@@ -1,9 +1,7 @@
-/*
-Copyright © 2013 Kyle Mahan <kyle.mahan@gmail.com>
-This work is free. You can redistribute it and/or modify it under the
-terms of the Do What The Fuck You Want To Public License, Version 2,
-as published by Sam Hocevar. See the COPYING file for more details.
-*/
+#Copyright © 2013 Kyle Mahan <kyle.mahan@gmail.com>
+#This work is free. You can redistribute it and/or modify it under the
+#terms of the Do What The Fuck You Want To Public License, Version 2,
+#as published by Sam Hocevar. See the COPYING file for more details.
 
 import dropbox.client, dropbox.session, dropbox.rest
 import pelican
